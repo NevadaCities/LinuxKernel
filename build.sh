4765bb6408d2cea04d650b43a1b055f8d1823dd2
LinBeitsi <58358419+NevadaCities@users.noreply.github.com>
2024-01-26 15:51:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-
 sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt full-upgrade --assume-yes
 sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes wget bc binutils bison dwarves flex gcc git gnupg2 gzip libelf-dev libncurses5-dev libssl-dev make openssl pahole perl-base rsync tar xz-utils build-essential kmod cpio libncurses5-dev libelf-dev libssl-dev dwarves
 cd "${GITHUB_WORKSPACE}"
-
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.7.2.tar.xz
-tar -xvf ./linux-6.7.2.tar.xz
-
+tar -xf ./linux-6.7.2.tar.xz
 cd linux-6.7.2
-
 cp ../config .config
+make olddefconfig
 scripts/config --disable DEBUG_INFO
-
 sudo make -j`nproc` bindeb-pkg
-
 cd ..
 mkdir "artifact"
 mv ./linux*.deb ./artifact/
