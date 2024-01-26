a125251d84c9c2c5cadfeca091c5df03c828b53b
LinBeitsi <58358419+NevadaCities@users.noreply.github.com>
2024-01-26 15:43:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-sudo apt update && sudo apt dist-upgrade -y
-sudo apt install -y wget build-essential flex bison libssl-dev libelf-dev
+sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt full-upgrade --assume-yes
+sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes wget bc binutils bison dwarves flex gcc git gnupg2 gzip libelf-dev libncurses5-dev libssl-dev make openssl pahole perl-base rsync tar xz-utils build-essential kmod cpio libncurses5-dev libelf-dev libssl-dev dwarves
 cd "${GITHUB_WORKSPACE}"
 
-wget https://git.kernel.org/torvalds/t/linux-6.0-rc3.tar.gz > /dev/null 2>&1
-tar zxvf ./linux-6.0-rc3.tar.gz > /dev/null 2>&1
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.7.2.tar.xz
+tar -xvf ./linux-6.7.2.tar.xz
 
-cd linux-6.0-rc3
+cd linux-6.7.2
 
 cp ../config .config
 scripts/config --disable DEBUG_INFO
 
-sudo make deb-pkg
+sudo make -j`nproc` bindeb-pkg
 
 cd ..
 mkdir "artifact"
-mv ./linux*.deb artifact/
+mv ./linux*.deb ./artifact/
